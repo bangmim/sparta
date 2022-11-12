8551bcfc51254cbe09d997d7c5621bb7305638c7
bangmim <amd_b@naver.com>
2022-11-13 02:55:29 +0900

    5일차

--- javaKorea/src/day04/Flow3.java
+++ javaKorea/src/day04/Flow3.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Flow3 {
 	public static void main(String[] args) {
 		//퀴즈3
-		java.util.Scanner sc=new Scanner(System.in);
+		
+		Scanner sc=new Scanner(System.in);		//입력받는 프로그램(Java.util)
 		String answer=null;
 		while(true) {
-			System.out.println("대한민국의 수도는?");
+			System.out.println("대한민국의 수도는?");	
 			answer=sc.next();
-			if(answer.equals("서울")) {
+			if(answer.equals("서울")) {			//String 문자열 비교는 == 사용 불가 -> .equals
 				System.out.println("정답입니다");
 				break;
 			} else {
@@ -19,4 +20,5 @@ public class Flow3 {
 			}
 		}
 	}
+	
 }
